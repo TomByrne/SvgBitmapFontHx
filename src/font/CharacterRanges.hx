a0e4fb5108209482d497ad87ae81ad548d6ffbd0
Tom Byrne <tom.byrne@imagination.com>
2020-09-21 10:29:31 +1000

    Added additional character ranges

--- src/font/CharacterRanges.hx
+++ src/font/CharacterRanges.hx
@@ -22,6 +22,20 @@ class CharacterRanges
 	public static var LATIN1_LOWERCASE:Array<Int> = [223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 248, 249, 250, 251, 252, 253, 254, 255];//223-246, 248-255
 	public static var LATIN_SUPPLEMENT:Array<Int> = LATIN1_UPPERCASE.concat(LATIN1_LOWERCASE);
 
+	//based on https://www.rapidtables.com/code/text/unicode-characters.html
+	// copyright, registered trademark, sound recording copyright, trademark, service mark
+	public static var INTELLECTUAL_PROPERTY:Array<Int> = [169, 174, 8471, 8482, 8480];
+	
+	public static var GREEK_ALPHABET:Array<Int> = createRange(913, 969);
+	
+	// dollar, euro, pound, yen / yuan, cent, indian Rupee, rupee, peso, korean won, thai baht, dong, shekel
+	public static var CURRENCY_CODES:Array<Int> = [36, 8364, 163, 165, 162, 8377, 8360, 8369, 8361, 3647, 8363, 8362];
+	
+	// horizontal tab, line feed, carriage return / enter, non-breaking space
+	public static var SPECIAL_CODES:Array<Int> = [9, 10, 13, 160];
+
+
+
 	static function createRange(start:Int, end:Int) : Array<Int>
 	{
 		var ret = [];
@@ -62,6 +76,11 @@ class CharacterRanges
 		'unicodeSymbols' => UNICODE_SYMBOLS,
 		'unicodeSymbols.basic' => UNICODE_SYMBOLS_BASIC,
 		'unicodeSymbols.extended' => UNICODE_SYMBOLS_EXTENDED,
+
+		'intellectualProperty' => INTELLECTUAL_PROPERTY,
+		'greek.alphabet' => GREEK_ALPHABET,
+		'currencyCodes' => CURRENCY_CODES,
+		'specialCodes' => SPECIAL_CODES,
 	];
 	
 	public static function fromStrings(strs:Array<String>):Array<Int>
